3beab8c509d7828703765a2c3bd15a2c6038b837
Emmanuel <Emmanuel.csh@hotmail.com>
2025-01-23 21:20:44 +0800

    Level-1

--- src/main/java/BuddyTalk.java
+++ src/main/java/BuddyTalk.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class BuddyTalk {
     public static void main(String[] args) {
-        String para = """
+        String start = """
                 ____________________________________________________________
                 Hello! I'm BuddyTalk
                 What can I do for you?
                 ____________________________________________________________
+                """;
+
+        String bye = """
+                ____________________________________________________________
                 Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
-        System.out.println(para);
+        System.out.println(start);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                exit = true;
+                System.out.println(bye);
+            } else {
+                String text = String.format("""
+                        ____________________________________________________________
+                        %s
+                        ____________________________________________________________
+                        """, input);
+                System.out.println(text);
+            }
+        }
     }
 }
