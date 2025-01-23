709ef56c7656d5b2f394e1b378536201dde0a9fc
Emmanuel <Emmanuel.csh@hotmail.com>
2025-01-23 21:50:31 +0800

    Level-2

--- src/main/java/BuddyTalk.java
+++ src/main/java/BuddyTalk.java
@@ -1,19 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BuddyTalk {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
         String start = """
                 ____________________________________________________________
-                Hello! I'm BuddyTalk
-                What can I do for you?
+                 Hello! I'm BuddyTalk
+                 What can I do for you?
                 ____________________________________________________________
                 """;
+        start = start.indent(4);
 
         String bye = """
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
+                 Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
+        bye = bye.indent(4);
+
+        String bar = "____________________________________________________________";
+        bar = bar.indent(4);
+
         System.out.println(start);
 
         Scanner scanner = new Scanner(System.in);
@@ -23,12 +32,21 @@ public class BuddyTalk {
             if (input.equals("bye")) {
                 exit = true;
                 System.out.println(bye);
+            } else if (input.equals("list")){
+                System.out.print(bar);
+                for (int i = 0; i < list.size(); i++) {
+                    int j = i + 1;
+                    System.out.println("     " + j + "." + " " + list.get(i));
+                }
+                System.out.println(bar);
             } else {
+                list.add(input);
                 String text = String.format("""
                         ____________________________________________________________
-                        %s
+                         added: %s
                         ____________________________________________________________
                         """, input);
+                text = text.indent(4);
                 System.out.println(text);
             }
         }
