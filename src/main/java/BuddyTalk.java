9c4b45bd1a235e0301fbc92a07f4c919cab89969
Emmanuel <Emmanuel.csh@hotmail.com>
2025-01-23 20:41:51 +0800

    Level-0

---
+++ src/main/java/BuddyTalk.java
@@ -0,0 +1,11 @@
+public class BuddyTalk {
+    public static void main(String[] args) {
+        String text = "____________________________________________________________\n"
+                + "Hello! I'm BuddyTalk\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(text);
+    }
+}
